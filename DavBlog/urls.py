f0f33c5c07bbbf9a6ea23761ce2715bf8d187b5b
Davisme2 <davis93m@gmail.com>
2022-11-03 18:31:59 +0000

    Homepage making

--- DavBlog/urls.py
+++ DavBlog/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import Blog.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', ),
+    path('', views.home, name="home"),
 ]
