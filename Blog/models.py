48cd57cf491635496db501a9509d35a8318e51f8
Davisme2 <davis93m@gmail.com>
2022-11-07 04:00:10 +0000

    frontend detail page

--- Blog/models.py
+++ Blog/models.py
@@ -7,9 +7,17 @@ STATUS = {
 }
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     image = models.ImageField()
