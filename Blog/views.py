94023b1dffb83674c20df205980b101d3b4e3178
Davisme2 <davis93m@gmail.com>
2022-11-09 01:05:03 +0000

    Fix Sidbar bugs

--- Blog/views.py
+++ Blog/views.py
@@ -10,8 +10,14 @@ def PostList(request):
 
 
 def PostDetail(request, slug):
+    # Detail article
     detail = Post.objects.get(slug=slug)
-    context = {"detail": detail}
+    category = detail.category
+    recetteEnRelation = Post.objects.filter(category_id=category)
+    # print(recetteRelation) liste de plat en relation
+
+    # Liste de toutes le categories de plat
+    cat = Category.objects.all()
+
+    context = {"detail": detail, "recetteEnRelation": recetteEnRelation, "cat": cat}
     return render(request, "Blog/post_detail.html", context)
-
-
